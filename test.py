290d555c654a52d06bab739ecad7e3a7d6bab798
23xiao <3283827735@qq.com>
2026-05-04 23:03:46 +0800

    feat: 完成商家展示页面，支持搜索、筛选、查看菜单功能

---
+++ test.py
@@ -0,0 +1,9 @@
+# 快速排序函数
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]  # 选择中间元素作为基准
+    left = [x for x in arr if x < pivot]  # 小于基准的元素
+    middle = [x for x in arr if x == pivot]  # 等于基准的元素
+    right = [x for x in arr if x > pivot]  # 大于基准的元素
+    return quick_sort(left) + middle + quick_sort(right)  # 递归排序并合并结果
